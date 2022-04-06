d6af8b1ed22f56c7b94b9a4005290e6932f913cb
CBX0N <88788571+CBX0N@users.noreply.github.com>
2022-04-06 20:42:22 +0100

    Purging old files

--- install/vs_certs.ps1
+++
@@ -1,4 +0,0 @@
-$certpath = "C:\localVSlayout\certificates"
-certutil.exe -addstore -f "Root" "$certpath\manifestRootCertificate.cer"
-certutil.exe -addstore -f "Root" "$certpath\certificates\manifestCounterSignRootCertificate.cer"
-certutil.exe -addstore -f "Root" "$certpath\certificates\vs_installer_opc.RootCertificate.cer"
